6b7eba30e99ecdec9c648bc5af9a8305291062cb
Shivas Jayaram <shivasj@gmail.com>
2023-09-27 13:54:57 -0400

    script file to create ai platform jobs

---
+++ cli.sh
@@ -0,0 +1,22 @@
+
+export UUID=$(openssl rand -hex 6)
+export DISPLAY_NAME="mushroom_training_job_$UUID"
+export MACHINE_TYPE="n1-standard-4"
+export REPLICA_COUNT=1
+export EXECUTOR_IMAGE_URI="us-docker.pkg.dev/vertex-ai/training/tf-gpu.2-12.py310:latest"
+export PYTHON_PACKAGE_URI=$GCS_BUCKET_URI/mushroom-app-trainer.tar.gz
+export PYTHON_MODULE="trainer.task"
+export ACCELERATOR_TYPE="NVIDIA_TESLA_T4"
+export ACCELERATOR_COUNT=1
+
+#export CMDARGS="--model_name=mobilenetv2,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
+#export CMDARGS="--model_name=mobilenetv2,--train_base,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
+#export CMDARGS="--model_name=tfhub_mobilenetv2,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
+export CMDARGS="--model_name=tfhub_mobilenetv2,--train_base,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
+
+gcloud ai custom-jobs create \
+  --region=$GCP_REGION \
+  --display-name=$DISPLAY_NAME \
+  --python-package-uris=$PYTHON_PACKAGE_URI \
+  --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,accelerator-type=$ACCELERATOR_TYPE,accelerator-count=$ACCELERATOR_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
+  --args=$CMDARGS
