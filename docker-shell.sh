dc7b649948330225e196d985e39c478618158545
Shivas Jayaram <shivasj@gmail.com>
2023-09-27 13:52:25 -0400

    updated docker shell and entrypoint

--- docker-shell.sh
+++ docker-shell.sh
@@ -5,20 +5,23 @@ set -e
 export IMAGE_NAME=model-training-cli
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="mushroom-app-data-demo"
-#export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_URI="gs://mushroom-app-trainer"
 export GCP_PROJECT="mlproject01-207413"
-export GCP_ZONE="us-central1-a"
+export GCP_REGION="us-central1"
+
 
 # Build the image based on the Dockerfile
+#docker build -t $IMAGE_NAME -f Dockerfile .
+# M1/2 chip macs use this line
 docker build -t $IMAGE_NAME --platform=linux/arm64/v8 -f Dockerfile .
 
 # Run Container
 docker run --rm --name $IMAGE_NAME -ti \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
--e GOOGLE_APPLICATION_CREDENTIALS=/secrets/deployment.json \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/model-trainer.json \
 -e GCP_PROJECT=$GCP_PROJECT \
--e GCP_ZONE=$GCP_ZONE \
--e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+-e GCP_REGION=$GCP_REGION \
+-e GCS_BUCKET_URI=$GCS_BUCKET_URI \
+-e WANDB_KEY=$WANDB_KEY \
 $IMAGE_NAME
